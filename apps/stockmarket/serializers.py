16905c5747501b030a293195e8effa33df05dbf1
Zach <zrichardson1699@yahoo.com>
2015-10-20 20:31:27 -0600

    updated sprint backlog (final version)

--- apps/stockmarket/serializers.py
+++ apps/stockmarket/serializers.py
@@ -5,8 +5,11 @@ from apps.stockmarket.models import Stock, Price
 class StockSerializer(serializers.ModelSerializer):
     class Meta:
         model = Stock
-
+        field = ("id", "name")
 
 class PriceSerializer(serializers.ModelSerializer):
+    stock = StockSerializer()
+
     class Meta:
         model = Price
+        field = ("id", "stock", "date")

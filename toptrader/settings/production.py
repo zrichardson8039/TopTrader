05780301c091c2e9cdeb824b44f722a0a383fdf6
Zach <zrichardson1699@yahoo.com>
2015-10-21 20:39:37 -0600

    updated production with DATABASES
    Signed-off-by: Zach <zrichardson1699@yahoo.com>

--- toptrader/settings/production.py
+++ toptrader/settings/production.py
@@ -1,5 +1,5 @@
 import os
-from django.conf import settings
+from django.conf import settings, global_settings
 import dj_database_url
 
 # Parse database configuration from $DATABASE_URL
@@ -8,7 +8,16 @@ import dj_database_url
 DEBUG = False
 TEMPLATE_DEBUG = False # change this to false eventually
 
-DATABASES['default'] =  dj_database_url.config()
+DATABASES = {
+        'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'toptrader_dev',
+        'USER': 'toptrader_dev',
+        'PASSWORD': 'toptrader_dev',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')

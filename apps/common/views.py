419acd0c09549512f74096515d8695d60fa7e80b
Zach <zrichardson1699@yahoo.com>
2015-11-04 01:20:33 -0700

    fixed indentation

--- apps/common/views.py
+++ apps/common/views.py
@@ -17,17 +17,17 @@ def registration(request):
     if request.user.is_authenticated():
         return HttpResponseRedirect('/profile/')
     form = RegistrationForm(request.POST or None)
-        if form.is_valid():
-            first_name = form.cleaned_data['first_name']
-            last_name = form.cleaned_data['first_name']
-            username = form.cleaned_data['first_name']
-            email = form.cleaned_data['first_name']
-            password = form.cleaned_data['first_name']
-            user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
-            user.save()
-            return HttpResponseRedirect('/profile/')
-        else:
-            return render(request, "registration.html", {})
+    if form.is_valid():
+        first_name = form.cleaned_data['first_name']
+        last_name = form.cleaned_data['first_name']
+        username = form.cleaned_data['first_name']
+        email = form.cleaned_data['first_name']
+        password = form.cleaned_data['first_name']
+        user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
+        user.save()
+        return HttpResponseRedirect('/profile/')
+    else:
+        return render(request, "registration.html", {})
     else:
         form = RegistrationForm()
         context = {"form": form}

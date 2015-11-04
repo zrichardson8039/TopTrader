635708c509ce514a56da788b13ec42d649a3c391
Zach <zrichardson1699@yahoo.com>
2015-11-04 01:33:06 -0700

    Hopefully corrected registration form

--- apps/common/views.py
+++ apps/common/views.py
@@ -19,10 +19,10 @@ def registration(request):
     form = RegistrationForm(request.POST or None)
     if form.is_valid():
         first_name = form.cleaned_data['first_name']
-        last_name = form.cleaned_data['first_name']
-        username = form.cleaned_data['first_name']
-        email = form.cleaned_data['first_name']
-        password = form.cleaned_data['first_name']
+        last_name = form.cleaned_data['last_name']
+        username = form.cleaned_data['username']
+        email = form.cleaned_data['email']
+        password = form.cleaned_data['password']
         user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
         user.save()
     context = {"form": form}
